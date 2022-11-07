23e78bbc17ec811f63bea195c839f995c5e7fb7e
DmEnd <Dima_polit-sov@mail.ru>
2022-11-08 02:14:34 +0300

    Добавьте файлы проекта.

---
+++ HomeWork12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1.   Разработать статический класс для работы с окружностью. Класс должен содержать 3 метода:
+                метод, определяющий длину окружности по заданному радиусу;
+                метод, определяющий площадь круга по заданному радиусу;
+                метод, проверяющий принадлежность точки с координатами (x,y) кругу с радиусом r и координатами центра x0, y0.
+             */
+            Console.Write("Введите радиус r круга. r = ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nДлина окружности равна {0}.", Сircle.СircleLength(r));
+            Console.WriteLine("Площадь круга равна {0}.", Сircle.СircleArea(r));
+
+            Console.Write("\nВведите коодинату x точки. x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите коодинату y точки. y = ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\nВведите коодинату x0 центра круга. x0 = ");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите коодинату y0 центра круга. y0 = ");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Сircle.PointBelongСircle(x, y, r, x0, y0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork12/Сircle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork12
+{
+    static class Сircle
+    {
+       static public double СircleLength(double r)
+       {
+            return 2 * r * Math.PI;
+       }
+        static public double  СircleArea(double r)
+        {
+            return r * r * Math.PI;
+        }
+        static public void PointBelongСircle( double x, double y,double r, double x0, double y0)
+        {
+            if (Math.Sqrt(Math.Pow(x-x0,2)+ Math.Pow(y - y0, 2)) <r)
+            {
+                Console.WriteLine("Точка с координатами ({0},{1}) принадлежит кругу с центром в ({2},{3}) и радиусом {4}.", x,y, x0, y0,r);
+            }
+            else
+            {
+                Console.WriteLine("Точка с координатами ({0},{1}) НЕ принадлежит кругу с центром в ({2},{3}) и радиусом {4}.", x, y, x0, y0, r);
+            }
+            
+        }
+    }
+}
